358f4bdcd202f919daa8bf7823f23cd3cb8f76e2
JimmyStarling <JimmyStarlingDev@gmail.com>
2022-04-27 02:27:42 -0300

    Add LoginRepository.

--- lib/features/domain/repositories/login_repository.dart
+++ lib/features/domain/repositories/login_repository.dart
@@ -1,8 +1,11 @@
 import 'package:dartz/dartz.dart';
-
-import '../../../../core/error/failures.dart';
-import 'package:framework_digital_ecommerce/features/domain/entities/login_entity.dart';
+import 'package:framework_digital_ecommerce/core/exceptions/failures.dart';
+import 'package:framework_digital_ecommerce/features/domain/entities/user_info_entity.dart';
 
 abstract class LoginRepository {
-  Future<Either<Failure, bool>> login(LoginCredentials login);
+  Future<Either<Failure, LoggedUserInfo>> verifyAuthCode({String verificationId, String code});
+  Future<Either<Failure, LoggedUserInfo>> loginEmail({String? email, String? password});
+  Future<Either<Failure, LoggedUserInfo>> loginPhone({String phoneNumber});
+  Future<Either<Failure, LoggedUserInfo>> loggedUser();
+  Future<Either<Failure, Unit>> logout();
 }
