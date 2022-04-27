ccac6cf8d28dbcc58484890cfac227df809bf3c1
JimmyStarling <JimmyStarlingDev@gmail.com>
2022-04-27 02:30:03 -0300

    Add new featured screen.

---
+++ lib/features/presentation/screens/process/forgot_password.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ForgotPasswordScreen extends StatelessWidget {
+  const ForgotPasswordScreen({Key? key}) : super(key: key);
+  static String routeName = "/forgot-password";
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
