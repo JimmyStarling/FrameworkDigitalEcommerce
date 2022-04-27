2f0ff4cfe4b9b479ef404324ee32bb2fc3759b4c
JimmyStarling <JimmyStarlingDev@gmail.com>
2022-04-26 23:18:45 -0300

    Add screens.

---
+++ lib/features/presentation/screens/home/home_screen.dart
@@ -0,0 +1,37 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:framework_digital_ecommerce/features/presentation/screens/home/components/body.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Body(),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Color(0xFF6200EE),
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white.withOpacity(.60),
+        selectedFontSize: 14,
+        unselectedFontSize: 14,
+        onTap: (value) {
+          // Respond to item press.
+        },
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline_rounded),
+          ),
+        ]
+      ),
+    );
+  }
+}
