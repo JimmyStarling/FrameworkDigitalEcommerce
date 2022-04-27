f797bf318c42f1c15402e7b887be05e8f55561db
JimmyStarling <JimmyStarlingDev@gmail.com>
2022-04-27 03:10:15 -0300

    Organize into scopes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:framework_digital_ecommerce/app/framework_digital_ecommerce.dart';
 
-void main() => runApp(const App());
+void main() => {
+  runApp(const App())
+};
